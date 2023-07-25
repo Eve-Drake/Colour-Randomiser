9f58b80d2ee63c3311aa388eb11199631206bfda
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-07-25 13:12:34 +0100

    Random colour added, basic styling

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-import './style.css'
+const button = document.getElementById('change-colour')
+const currentColour = document.querySelector('.colour-hex')
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+button.addEventListener('click', function(){
+  let value1 = Math.floor(Math.random() * 255)
+  let value2 = Math.floor(Math.random() * 255)
+  let value3 = Math.floor(Math.random() * 255)
+  document.body.style.backgroundColor = `rgba(${value1},${value2},${value3})`
+  currentColour.textContent = `rgb(${value1},${value2},${value3})`
+})
